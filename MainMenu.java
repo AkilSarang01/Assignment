786f0ba11547fd6691afd4ec9e583e45fa31f51d
AkilSarang01 <86415344+AkilSarang01@users.noreply.github.com>
2021-06-30 01:00:28 +0530

    Add files via upload

---
+++ MainMenu.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class MainMenu
+{
+  public void menu()
+  {
+    
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("\t\t*******************************************");
+    
+    System.out.println("\n\nPress 1 : To Add an Employee Details");
+    System.out.println("Press 2 : To See an Employee Details ");
+    System.out.println("Press 3 : To Remove an Employee");
+  }
+}
